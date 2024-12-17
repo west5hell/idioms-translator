26aaba762cc77381875eaa6321eecc23e6c33bc7
Pongter Chia <dozeuto@gmail.com>
2024-12-18 00:53:51 +0800

    deepseek api files

---
+++ app.py
@@ -0,0 +1,59 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect, url_for, session
+
+from utilities import deepseek
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+
+# @app.route("/", methods=["GET", "POST"])
+# def index():
+#     title = "英语习语翻译"
+#     heading = "英语习语翻译"
+#     user_input = ""
+#     output_text = ""
+
+#     if request.method == "POST":
+#         user_input = request.form.get("user_input", "")
+#         output_text = deepseek.translate(user_input)
+
+#     return render_template(
+#         "index.html",
+#         title=title,
+#         heading=heading,
+#         user_input=user_input,
+#         output_text=output_text,
+#     )
+
+
+@app.route("/", methods=["GET", "POST"])
+async def index():
+    title = "英语习语翻译"
+    heading = "英语习语翻译"
+    # user_input = ""
+    # output_text = ""
+
+    if request.method == "POST":
+        user_input = request.form.get("user_input", "")
+        output_text = await deepseek.translate_async(user_input)
+
+        session["output_text"] = output_text
+        session["user_input"] = user_input
+
+        return redirect(url_for("index"))
+
+    output_text = session.pop("output_text", "")
+    user_input = session.pop("user_input", "")
+
+    return render_template(
+        "index.html",
+        title=title,
+        heading=heading,
+        user_input=user_input,
+        output_text=output_text,
+    )
---
+++ utilities/deepseek.py
@@ -0,0 +1,38 @@
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI, AsyncOpenAI
+
+load_dotenv()
+
+api_key = os.environ.get("DEEPSEEK_API_KEY")
+base_url = os.environ.get("DEEPSEEK_BASE_URL")
+
+client = OpenAI(api_key=api_key, base_url=base_url)
+client_async = AsyncOpenAI(api_key=api_key, base_url=base_url)
+
+
+def translate(source_text: str):
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant"},
+            {"role": "user", "content": source_text},
+        ],
+        stream=False,
+    )
+
+    return response.choices[0].message.content
+
+
+async def translate_async(input_text: str):
+    response = await client_async.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant"},
+            {"role": "user", "content": input_text},
+        ],
+        stream=False,
+    )
+
+    return response.choices[0].message.content
